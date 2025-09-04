347a8afa3510facd901d5043cd1e40c6b62fc87f
makdeniss <denissmakarenkovsp@Mac.lan>
2025-09-04 12:22:24 +0300

    fix: add missing iam graphql service (#2)

--- src/portal-options/index.ts
+++ src/portal-options/index.ts
@@ -3,3 +3,4 @@ export * from './portal-context-provider/openmfp-portal-context.service.js';
 export * from './request-context-provider/openmfp-request-context-provider.js';
 export * from './service-providers/content-configuration-service-providers.service.js';
 export * from './service-providers/iam/auth-callback-provider.js';
+export * from './service-providers/iam/iam-graphql.service.js';
