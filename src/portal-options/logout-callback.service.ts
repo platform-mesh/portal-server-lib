c743db8f771eadbad2e78db240f515ac070c2d21
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2025-10-17 11:42:59 +0200

    Refactor and test logout callback service for Keycloak integration

--- src/portal-options/logout-callback.service.ts
+++ src/portal-options/logout-callback.service.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { HttpException, Inject, Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import {
   AUTH_CONFIG_INJECTION_TOKEN,
   AuthConfigService,
@@ -13,9 +13,6 @@ import { firstValueFrom } from 'rxjs';
 export class PMLogoutService implements LogoutCallback {
   private logger: Logger = new Logger(PMLogoutService.name);
 
-  private realmLogoutUrl =
-    'https://portal.cc-d2.showroom.apeirora.eu/keycloak/realms/default/protocol/openid-connect/logout';
-
   constructor(
     @Inject(AUTH_CONFIG_INJECTION_TOKEN)
     private authConfigService: AuthConfigService,
@@ -26,24 +23,10 @@ export class PMLogoutService implements LogoutCallback {
   public async handleLogout(
     request: Request,
     response: Response,
-  ): Promise<void> {
+  ): Promise<void | string> {
+    const authConfig = await this.authConfigService.getAuthConfig(request);
     try {
       const refreshToken = this.cookiesService.getAuthCookie(request);
-      const authConfig = await this.authConfigService.getAuthConfig(request);
-
-      if (!refreshToken) {
-        this.logger.warn(
-          'No refresh token found — falling back to front-channel logout',
-        );
-
-        const idToken = request.headers['authorization']?.split(' ')[1];
-        const postLogoutRedirect = `/login`;
-        const idTokenHintParam = `id_token_hint=${encodeURIComponent(idToken)}`;
-        const postLogoutRedirectUri = `post_logout_redirect_uri=${encodeURIComponent(postLogoutRedirect)}`;
-        const logoutRedirectUrl = `${this.realmLogoutUrl}?${idTokenHintParam}&${postLogoutRedirectUri}`;
-
-        return response.redirect(logoutRedirectUrl);
-      }
 
       const body = new URLSearchParams({
         client_id: authConfig.clientId,
@@ -58,13 +41,20 @@ export class PMLogoutService implements LogoutCallback {
       );
     } catch (error: any) {
       this.logger.error(
-        'Error during Keycloak logout',
+        'Error during keycloak logout',
         error?.response?.data || error.message,
       );
-      throw new HttpException(
-        error?.response?.data || 'Logout failed',
-        error?.response?.status || 500,
-      );
+      this.logger.warn('Trying to log out with the id token');
+      return this.logoutWithIdToken(request, authConfig.endSessionUrl);
     }
+  }
+
+  private logoutWithIdToken(request: Request, endSessionUrl: string) {
+    const { id_token_hint, post_logout_redirect_uri } = request.query;
+    const params = new URLSearchParams({
+      id_token_hint: String(id_token_hint),
+      post_logout_redirect_uri: String(post_logout_redirect_uri),
+    });
+    return `${endSessionUrl}?${params}`;
   }
 }
