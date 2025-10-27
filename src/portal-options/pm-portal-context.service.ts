a7282933f06dd98037d1f34d36f0cc02db6e9e20
Sobyt483 <kravczov.andrian@mail.ru>
2025-10-27 16:51:13 +0400

    feat: cleanup PMPortalContextService

--- src/portal-options/pm-portal-context.service.ts
+++ src/portal-options/pm-portal-context.service.ts
@@ -7,23 +7,10 @@ import process from 'node:process';
 
 @Injectable()
 export class PMPortalContextService implements PortalContextProvider {
-  private readonly openmfpPortalContext = 'OPENMFP_PORTAL_CONTEXT_';
-
   constructor(private kcpKubernetesService: KcpKubernetesService) {}
 
   getContextValues(request: Request): Promise<Record<string, any>> {
     const portalContext: Record<string, any> = {};
-
-    const keys = Object.keys(process.env).filter((item) =>
-      item.startsWith(this.openmfpPortalContext),
-    );
-    keys.forEach((key) => {
-      const keyName = key.substring(this.openmfpPortalContext.length).trim();
-      if (keyName.length > 0) {
-        const camelCaseName = this.toCamelCase(keyName);
-        portalContext[camelCaseName] = process.env[key];
-      }
-    });
 
     this.processGraphQLGatewayApiUrl(request, portalContext);
     this.addKcpWorkspaceUrl(request, portalContext);
@@ -48,21 +35,5 @@ export class PMPortalContextService implements PortalContextProvider {
     portalContext.crdGatewayApiUrl = portalContext.crdGatewayApiUrl
       ?.replace('${org-subdomain}', subDomain)
       .replace('${org-name}', org);
-  }
-
-  private toCamelCase(text: string): string {
-    let firstSegment = true;
-    const items = text.split('_').map((item) => {
-      if (firstSegment) {
-        firstSegment = false;
-        return item.toLowerCase();
-      }
-      return this.capitalizeFirstLetter(item.toLowerCase());
-    });
-    return items.join('');
-  }
-
-  private capitalizeFirstLetter(text: string): string {
-    return String(text).charAt(0).toUpperCase() + String(text).slice(1);
   }
 }
